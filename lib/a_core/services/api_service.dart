32a35e40d6d1f1d7be8fb3cda378680de5b5491c
asfateen <ahmed.seif.fateen@gmail.com>
2024-12-15 13:41:05 +0200

    fix category

--- lib/a_core/services/api_service.dart
+++ lib/a_core/services/api_service.dart
@@ -1,21 +1,21 @@
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart' show debugPrint;
 import 'package:http/http.dart' as http;
 import 'package:batee5/a_core/constants/api_constants.dart';
-import 'package:batee5/a_core/models/category.dart';
+import 'package:batee5/a_core/models/category.dart' as models;
 import 'package:batee5/a_core/models/product.dart';
 
 class ApiService {
   final http.Client _client = http.Client();
 
-  Future<Map<String, Category>> getCategories() async {
+  Future<Map<String, models.Category>> getCategories() async {
     final response = await _client.get(Uri.parse('${ApiConstants.baseUrl}${ApiConstants.categories}'));
     
     if (response.statusCode == 200) {
       final Map<String, dynamic> data = json.decode(response.body);
       return data.map((key, value) => MapEntry(
         key,
-        Category.fromJson(value as Map<String, dynamic>),
+        models.Category.fromJson(value as Map<String, dynamic>),
       ));
     }
     throw Exception('Failed to load categories');
