ebe940a7aebbc93e3c811eefd3cc5e5784f0954d
asfateen <ahmed.seif.fateen@gmail.com>
2024-12-15 09:29:50 +0200

    rename

---
+++ lib/a_core/config/api_config.dart
@@ -0,0 +1,7 @@
+class ApiConfig {
+  static const String baseUrl = 'http://localhost:5000/api';
+  
+  // Endpoints
+  static const String products = '/products';
+  static const String categories = '/categories';
+}
