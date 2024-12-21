311e7d495d715665f23ecd3788d4a1dee3444519
asfateen <ahmed.seif.fateen@gmail.com>
2024-12-21 14:59:08 +0200

    backend url

--- lib/a_core/constants/api_constants.dart
+++ lib/a_core/constants/api_constants.dart
@@ -3,10 +3,17 @@ import 'dart:html';
 import 'package:http/http.dart' as http;
 
 class ApiConstants {
-  static const String baseUrl = String.fromEnvironment(
-    'API_URL',
-    defaultValue: 'http://localhost:5000',
-  );
+  static String get baseUrl {
+    // For web platform
+    if (window.location.href.contains('railway.app')) {
+      return const String.fromEnvironment(
+        'API_URL',
+        defaultValue: 'https://batee5-backend-production.up.railway.app'
+      );
+    }
+    // For local development
+    return 'http://localhost:5000';
+  }
   
   // Endpoints
   static const String categories = '/categories';
