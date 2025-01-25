e11841faa7fa7e5da6f8f524b34824dd62912b4f
asfateen <ahmed.seif.fateen@gmail.com>
2025-01-25 21:32:56 +0200

    Category filter

--- lib/a_core/services/api_service.dart
+++ lib/a_core/services/api_service.dart
@@ -1,15 +1,14 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:batee5/a_core/constants/api_constants.dart';
-import 'package:batee5/data/models/category.dart';
+import 'package:batee5/data/models/category.dart' as models;
 import 'package:batee5/data/models/product.dart';
-import 'package:flutter/foundation.dart' as foundation;
-import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart' show debugPrint;
 
 class ApiService {
   final http.Client _client = http.Client();
 
-  Future<Map<String, Category>> getCategories() async {
+  Future<Map<String, models.Category>> getCategories() async {
     try {
       final response = await _client
           .get(Uri.parse('${ApiConstants.baseUrl}${ApiConstants.categories}'));
@@ -18,16 +17,16 @@ class ApiService {
         final Map<String, dynamic> data = json.decode(response.body);
 
         // Debug print to see the response
-        foundation.debugPrint('Categories API Response: $data');
+        debugPrint('Categories API Response: $data');
 
         return data.map((key, value) => MapEntry(
               key,
-              Category.fromJson(value as Map<String, dynamic>),
+              models.Category.fromJson(value as Map<String, dynamic>),
             ));
       }
       throw Exception('Failed to load categories: ${response.statusCode}');
     } catch (e) {
-      foundation.debugPrint('Error in getCategories: $e');
+      debugPrint('Error in getCategories: $e');
       return {};
     }
   }
@@ -40,18 +39,14 @@ class ApiService {
 
       if (response.statusCode == 200) {
         final Map<String, dynamic> data = json.decode(response.body);
-
-        // Debug print to see the response
-        foundation.debugPrint('API Response: $data');
+        debugPrint('API Response: $data');
 
         final products = <String, Product>{};
-
         data.forEach((key, value) {
           try {
-            products[key] =
-                Product.fromJson(key, value as Map<String, dynamic>);
+            products[key] = Product.fromJson(key, value as Map<String, dynamic>);
           } catch (e) {
-            foundation.debugPrint('Error parsing product $key: $e');
+            debugPrint('Error parsing product $key: $e');
           }
         });
 
@@ -59,7 +54,7 @@ class ApiService {
       }
       throw Exception('Failed to load products: ${response.statusCode}');
     } catch (e) {
-      foundation.debugPrint('Error in getProductsByCategory: $e');
+      debugPrint('Error in getProductsByCategory: $e');
       return {};
     }
   }
